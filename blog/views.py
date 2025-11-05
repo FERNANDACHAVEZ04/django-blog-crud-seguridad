ad2e44ccb4cda9f313e5d17b476e6966fd470af3
FERNANDACHAVEZ04 <fernandachagac@gmail.com>
2025-11-05 09:11:47 -0600

    Fase 2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
 
-# Create your views here.
+# Vista para listar todos los posts
+class BlogListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = "posts"
+    login_url = "login"
+
+# Vista para mostrar un solo post
+class BlogDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
+    login_url = "login"
